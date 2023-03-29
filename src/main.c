8130c1bef6ec14250f8ea98ae9ca5784388f268e
adnan-mhd <adnanmohamad0606@gmail.com>
2023-03-29 13:13:53 +0200

    Added working example

--- src/main.c
+++ src/main.c
@@ -1,60 +1,44 @@
+#include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_timer.h>
-#include <SDL2/SDL_image.h>
 
-int main()
+int main(int argv, char** args)
 {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window *window = SDL_CreateWindow("Hello SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
+    bool isRunning = true;
+    SDL_Event event;
+
+    while (isRunning)
     {
-        printf("Failed to initialize the SDL2 library\n");
-        return -1;
-    }
-
-    SDL_Window *window = SDL_CreateWindow("SDL2 Window",
-                                          SDL_WINDOWPOS_CENTERED,
-                                          SDL_WINDOWPOS_CENTERED,
-                                          640, 480,
-                                          0);
-
-    if (!window)
-    {
-        printf("Failed to create window\n");
-        return -1;
-    }
-
-    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
-
-    if (!window_surface)
-    {
-        printf("Failed to get the surface from the window\n");
-        return -1;
-    }
-
-    SDL_Surface *image = SDL_LoadBMP("/resources/hello_world.bmp");
-
-    if (!image)
-    {
-        printf("Failed to load image\n");
-        return -1;
-    }
-
-    bool keep_window_open = true;
-    while (keep_window_open)
-    {
-        SDL_Event e;
-        while (SDL_PollEvent(&e) > 0)
+        while (SDL_PollEvent(&event))
         {
-            switch (e.type)
+            switch (event.type)
             {
             case SDL_QUIT:
-                keep_window_open = false;
+                isRunning = false;
                 break;
-            }
 
-            SDL_BlitSurface(image, NULL, window_surface, NULL);
-            SDL_UpdateWindowSurface(window);
+            case SDL_KEYDOWN:
+                if (event.key.keysym.sym == SDLK_ESCAPE)
+                {
+                    isRunning = false;
+                }
+            }
         }
+
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+        SDL_RenderPresent(renderer);
     }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
