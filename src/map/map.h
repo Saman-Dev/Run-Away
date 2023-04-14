dede143b9b4f492ae291dbb5374bfa4f17dd706d
Unicorn9061 <128430264+Unicorn9061@users.noreply.github.com>
2023-04-15 00:15:39 +0200

    .h filen till map.c

---
+++ src/map/map.h
@@ -0,0 +1,14 @@
+#ifndef map.h
+#define map.h
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int get_number_of_rows_in_map(void);
+int get_number_of_columns_in_map(void);
+int get_the_width_of_the_tiles(void);
+int get_the_height_of_the_tiles(void);
+int get_tile_information(int x, int y);
+bool encounters_forbidden_tile(int x, int y);
+
+#endif
