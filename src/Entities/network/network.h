d8b07c803292a710e12ad947f6ccb7c35ebd53a8
Saman-Dev <Saman.ahmed@email.com>
2023-05-11 18:30:48 +0200

    Update network.h
    Made changes to the network code, fixed networking issues on Windows OS and solved connectivity issues between clients.

--- src/Entities/network/network.h
+++ src/Entities/network/network.h
@@ -1,3 +1,7 @@
+#ifndef NETWORK_H
+#define NETWORK_H
+
+#include <SDL2/SDL.h>
 #include <SDL2/SDL_net.h>
 
 #include "../player/player.h"
@@ -21,6 +25,8 @@ typedef struct {
     Uint32 clientPort1; 
     Uint32 clientIP2;
     Uint32 clientPort2;
+	Uint32 clientIP3;
+    Uint32 clientPort3;
 } AddressBook;
 
 void setUpClient(Network *information, char IP_address[], int port);
@@ -28,11 +34,13 @@ void sendData(Network *information, Cargo *toSend, Player *playerX);
 static int checkDifference(Cargo *toSend, Player *playerX);
 static void prepareTransfer(Cargo *toSend, Player *playerX);
 static void commenceTransfer(Network *information, Cargo *toSend);
-void receiveData(Network information, Player *playerX, Player *playerY);
+void receiveData(Network *information, Player *player1, Player *player2, Player *player3);
 void setUpServer(Network *information, int port);
-void manageServerDuties(Network *information, AddressBook *record, Player *playerX, Player *playerY, Player host, Cargo *toSend);
+void manageServerDuties(Network *information, AddressBook *record, Player *player1, Player *player2, Player *player3, Cargo *toSend);
 void initiateAddressBook(AddressBook *record);
-static void registerSourceInformation(Network *information, AddressBook *record);
+static void registerSourceInformation(Network *information, AddressBook *record, Player *player1, Player *player2, Player *player3);
 static void sendServerCopy(Network *information, Uint32 clientIP, Uint32 clientPort, Player *host);
-static void sendHostPlayerPacket(Network *information, AddressBook *record, Cargo *toSend, Player host);
+static void sendHostPlayerPacket(Network *information, AddressBook *record, Cargo *toSend, Player *host);
 static void applyReceivedData(Player *player, Cargo *toSend);
+
+#endif
