3eb6f175ba5e872be64d2e43acff9f43e03f2b0e
abood <abooddaker@gmail.com>
2023-05-16 22:38:30 +0200

    Updated perks

--- src/Entities/perks/perks.c
+++ src/Entities/perks/perks.c
@@ -1,55 +1,62 @@
 #include "perks.h"
 
-Perk createSpeedPerk(SDL_Renderer *renderer, char* spritePath, int posX, int posY, int duration){
-    Perk perk;
-    perk.texture = IMG_LoadTexture(renderer, spritePath);
-    if(perk.texture == NULL){
-        printf("Error loading texture: %s\n", SDL_GetError());
-        exit(1);
+void applySpeedBoostPerk(Player *player, SpeedBoostPerk *perk)
+{
+    if (perk->available && checkCollision(player->position, perk->rect)) 
+    {
+        player->speed += SPEED_BOOST_AMOUNT;
+        perk->available = false;
     }
 
-    perk.position.x = posX;
-    perk.position.y = posY;
-    SDL_QueryTexture(perk.texture, NULL, NULL, &perk.position.w, &perk.position.h);
-
-    perk.active = true;
-    perk.duration = duration;
-
-    return perk;
-}
-
-void renderPerk(SDL_Renderer *renderer, Perk *perk){
-    if(perk->active){
-        SDL_RenderCopy(renderer, perk->texture, NULL, &perk->position);
+    if (!perk->available) 
+    {
+        return;
     }
 }
 
-void applyPerk(Perk *perk, Player *player){
-    if(perk->active){
-        player->speed *= SPEED_BOOST_FACTOR;
-        perk->active = false;
-    }
-}
 
-void checkPerkCollision(Player *player, Perk *perk){
-    if(perk->active){
-        if(SDL_HasIntersection(&player->position, &perk->position)){
-            applyPerk(perk, player);
-        }
-    }
-}
-
-void decrementPerkDuration(Perk *perk, Player *player){
-    if(perk->duration > 0){
-        perk->duration--;
-    }
-    else if(perk->duration == 0){
-        player->speed /= SPEED_BOOST_FACTOR; // Reset speed
-        perk->duration = -1; // Set duration to -1 so this block won't run again
+void renderSpeedBoostPerk(SDL_Renderer *renderer, SpeedBoostPerk perk)
+{
+    if (perk.available)
+    {
+        SDL_RenderCopy(renderer, perk.texture, NULL, &perk.rect);
     }
 }
 
 bool checkCollision(SDL_Rect a, SDL_Rect b) // check perk/player collision
 {
     return (a.x + a.w > b.x && a.x < b.x + b.w) && (a.y + a.h > b.y && a.y < b.y + b.h);
+}
+
+SpeedBoostPerk initializeSpeedBoostPerk(SDL_Renderer *renderer) 
+{
+    SpeedBoostPerk speedBoostPerk;
+    SDL_Texture* perkTexture = IMG_LoadTexture(renderer, "resources/newperk.png");
+    if (perkTexture == NULL) {
+        printf("Failed to load perk sprite sheet: %s\n", IMG_GetError());
+        exit(1);
+    }
+
+    srand(time(NULL));
+    int randomLocation = rand() % 2; // generate random number to determine perk location between 2 spawnpoints
+
+    speedBoostPerk.texture = perkTexture;
+
+    if(randomLocation == 1) 
+    {
+        speedBoostPerk.rect.x = 435; 
+        speedBoostPerk.rect.y = 350; 
+    }
+    else 
+    {
+        speedBoostPerk.rect.x = 750; 
+        speedBoostPerk.rect.y = 750; 
+    }
+
+    speedBoostPerk.rect.w = PERK_WIDTH;  
+    speedBoostPerk.rect.h = PERK_HEIGHT; 
+    speedBoostPerk.available = true;
+    speedBoostPerk.duration = PERK_DURATION;
+
+    return speedBoostPerk;
 }
--- src/Entities/perks/perks.h
+++ src/Entities/perks/perks.h
@@ -1,25 +1,33 @@
 #ifndef PERKS_H
 #define PERKS_H
 
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
+
 #include "../player/player.h"
 
-#define SPEED_BOOST_FACTOR 2
+#define PERK_WIDTH 32
+#define PERK_HEIGHT 32
+#define PERK_DURATION 10
+#define SPEED_BOOST_AMOUNT 1
+#define SPEED_BOOST_DURATION 4
 
-typedef struct Perk {
-    SDL_Texture *texture;
-    SDL_Rect position;
-    bool active;
+typedef struct {
+    SDL_Texture* texture;
+    SDL_Rect rect;
+    bool available;
     int duration;
-} Perk;
+    int activationTime;
+} SpeedBoostPerk;
 
-Perk createSpeedPerk(SDL_Renderer *renderer, char* spritePath, int posX, int posY, int duration);
-void renderPerk(SDL_Renderer *renderer, Perk *perk);
-void applyPerk(Perk *perk, Player *player);
-void checkPerkCollision(Player *player, Perk *perk);
-void decrementPerkDuration(Perk *perk, Player *player);
+SpeedBoostPerk initializeSpeedBoostPerk(SDL_Renderer *renderer);
+void applySpeedBoostPerk(Player *player, SpeedBoostPerk *perk);
+void renderSpeedBoostPerk(SDL_Renderer *renderer, SpeedBoostPerk perk);
 bool checkCollision(SDL_Rect a, SDL_Rect b);
 
 
-#endif 
+#endif
+
